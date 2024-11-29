b63f91826aa085dd13b7b07c173cff1975bf0999
fstermann <fabian.stermann@gmail.com>
2024-11-29 09:24:11 +0100

    feat: add option to collect comments

--- soundcloud_tools/weekly.py
+++ soundcloud_tools/weekly.py
@@ -1,9 +1,11 @@
 import logging
 from datetime import datetime
+from typing import Literal
 
 import devtools
 
 from soundcloud_tools.client import Client
+from soundcloud_tools.models.comment import Comment
 from soundcloud_tools.models.playlist import PlaylistCreate
 from soundcloud_tools.models.request import PlaylistCreateRequest
 from soundcloud_tools.models.stream import Stream, StreamItem, StreamItemType
@@ -12,7 +14,18 @@ from soundcloud_tools.utils import Weekday, get_scheduled_time, get_week_of_mont
 logger = logging.getLogger(__name__)
 
 
+Items = StreamItemType | Literal["comment"]
+
+
 async def get_collections(
+    client: Client, user_id: int, start: datetime, end: datetime, exclude_own: bool = True
+) -> list[StreamItem | Comment]:
+    reposts = await get_reposts(client, user_id, start, end, exclude_own)
+    comments = await get_comments(client, user_id, start, end, exclude_own)
+    return reposts + comments
+
+
+async def get_reposts(
     client: Client, user_id: int, start: datetime, end: datetime, exclude_own: bool = True
 ) -> list[StreamItem]:
     limit, offset = 100, 0
@@ -30,10 +43,31 @@ async def get_collections(
         if not reposts:
             break
         offset += limit
-    return all_reposts
 
 
-def get_track_ids_from_collections(collections: list[StreamItem], types: list[StreamItemType]) -> set[int]:
+async def get_comments(
+    client: Client, user_id: int, start: datetime, end: datetime, exclude_own: bool = True
+) -> list[Comment]:
+    all_comments = []
+    followings = await client.get_user_followings_ids(user_id=user_id)
+    for user_id in followings.collection:
+        limit, offset = 100, 0
+        while True:
+            response: Stream = await client.get_user_comments(user_id=user_id, offset=offset)
+            comments = [
+                c
+                for c in response.collection
+                if start < c.created_at < end and (c.user.id != user_id if exclude_own else True)
+            ]
+            logger.info(f"Found {len(comments)} valid comments")
+            all_comments += comments
+            if not comments:
+                break
+            offset += limit
+    return all_comments
+
+
+def get_track_ids_from_collections(collections: list[StreamItem | Comment], types: list[Items]) -> set[int]:
     track_ids = set()
     for c in collections:
         if c.type not in types:
@@ -42,19 +76,24 @@ def get_track_ids_from_collections(collections: list[StreamItem], types: list[St
             track_ids |= {t.id for t in c.playlist.tracks}
         if c.type.startswith("track"):
             track_ids.add(c.track.id)
+        if c.type.startswith("comment"):
+            track_ids.add(c.track_id)
     return track_ids
 
 
-async def get_tracks_ids_in_timespan(
-    client: Client, user_id: int, start: datetime, end: datetime, types: list[StreamItemType]
-):
-    collections = await get_collections(client, user_id=user_id, start=start, end=end)
+async def get_tracks_ids_in_timespan(client: Client, user_id: int, start: datetime, end: datetime, types: list[Items]):
+    collections = []
+    if "track" in types or "track-repost" in types:
+        collections += await get_reposts(client, user_id, start=start, end=end, exclude_own=True)
+    if "comment" in types:
+        collections += await get_comments(client, user_id, start=start, end=end, exclude_own=True)
+
     track_ids = get_track_ids_from_collections(collections, types=types)
     logger.info(f"Found {len(track_ids)} tracks")
     return track_ids
 
 
-async def create_weekly_favorite_playlist(client: Client, user_id: int, types: list[StreamItemType], week: int = 0):
+async def create_weekly_favorite_playlist(client: Client, user_id: int, types: list[Items], week: int = 0):
     logger.info(f"Creating weekly favorite playlist for {week = } and {types = }")
     start = get_scheduled_time(Weekday.SUNDAY, weeks=week - 1)
     end = get_scheduled_time(Weekday.SUNDAY, weeks=week)
