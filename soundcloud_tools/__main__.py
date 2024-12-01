727f9b927e63e15f3ce12031eff0ae2946ebb5d1
fstermann <fabian.stermann@gmail.com>
2024-12-01 02:29:55 +0100

    fix: offset and add half schedule

--- soundcloud_tools/__main__.py
+++ soundcloud_tools/__main__.py
@@ -1,13 +1,14 @@
 import argparse
 import asyncio
 import logging
+from typing import Literal
 
 from soundcloud_tools.client import Client
 from soundcloud_tools.settings import get_settings
 from soundcloud_tools.weekly import create_weekly_favorite_playlist
 
 
-def main(week: int = 0):
+def main(week: int = 0, exclude_liked: bool = False, half: Literal["first", "second"] | None = None):
     logging.basicConfig(level=logging.INFO)
     asyncio.run(
         create_weekly_favorite_playlist(
@@ -15,6 +16,8 @@ def main(week: int = 0):
             user_id=get_settings().user_id,
             types=["track-repost", "track"],
             week=week,
+            exclude_liked=exclude_liked,
+            half=half,
         )
     )
 
@@ -22,8 +25,17 @@ def main(week: int = 0):
 def main_script():
     parser = argparse.ArgumentParser()
     parser.add_argument("--week", type=int, default=0)
+    parser.add_argument("--first", action="store_true")
+    parser.add_argument("--second", action="store_true")
+    parser.add_argument("--exclude-liked", action="store_true")
     args = parser.parse_args()
-    main(week=args.week)
+    if args.first and args.second:
+        raise ValueError("Cannot specify both first and second half")
+    main(
+        week=args.week,
+        exclude_liked=args.exclude_liked,
+        half="first" if args.first else "second" if args.second else None,
+    )
 
 
 if __name__ == "__main__":
